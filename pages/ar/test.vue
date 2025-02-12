1a7db7669fa5c07e7524de6d91eb0577be5977ec
bkmashiro <53376445+bkmashiro@users.noreply.github.com>
2025-02-12 15:40:44 +0000

    fix

--- pages/ar/test.vue
+++ pages/ar/test.vue
@@ -24,18 +24,11 @@ let renderer: THREE.WebGLRenderer | null
 let scene: THREE.Scene | null
 let camera: THREE.PerspectiveCamera | null
 
-async function activateXR() {
-  if (!canvas.value) {
-    throw new NotSupportedError("Canvas element not found.")
-  }
 
-  // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
-  gl = canvas.value.getContext("webgl2", { xrCompatible: true });
-
-  // To be continued in upcoming steps.
-}
 
 async function init() {
+  gl = canvas.value!.getContext("webgl2", { xrCompatible: true });
+
   scene = new THREE.Scene();
 
   // The cube will have a different color on each side.
@@ -53,115 +46,64 @@ async function init() {
   cube.position.set(1, 1, 1);
   scene.add(cube);
 
-  if (!gl) {
-    throw new NotSupportedError("WebGL context not found.")
-  }
-
-  if (!canvas.value) {
-    throw new NotSupportedError("WebXR not supported.")
-  }
-
   // Set up the WebGLRenderer, which handles rendering to the session's base layer.
   renderer = new THREE.WebGLRenderer({
     alpha: true,
     preserveDrawingBuffer: true,
-    canvas: canvas.value,
-    context: gl
+    canvas: canvas.value!,
+    context: gl!
   });
   renderer.autoClear = false;
-  renderer.xr.enabled = true;
 
   // The API directly updates the camera matrices.
   // Disable matrix auto updates so three.js doesn't attempt
   // to handle the matrices independently.
-  camera = new THREE.PerspectiveCamera();
+  const camera = new THREE.PerspectiveCamera();
   camera.matrixAutoUpdate = false;
 
-
-  console.log("Three.js components initialized.")
-
-
-  await initAR()
-}
-
-async function initAR() {
-  if (!gl) {
-    console.error("WebGL context not found.")
-    return
-  }
-
-  if (!navigator.xr) {
-    console.error("WebXR not supported.")
-    throw new NotSupportedError("WebXR not supported.")
-  }
   // Initialize a WebXR session using "immersive-ar".
-  const session = await navigator.xr.requestSession("immersive-ar");
+  const session = await navigator.xr!.requestSession("immersive-ar");
   session.updateRenderState({
-    baseLayer: new XRWebGLLayer(session, gl)
+    baseLayer: new XRWebGLLayer(session, gl!)
   });
 
   // A 'local' reference space has a native origin that is located
   // near the viewer's position at the time the session was created.
+  const referenceSpace = await session.requestReferenceSpace('local');
 
-  renderer!.xr.setSession(session);
   // Create a render loop that allows us to draw on the AR view.
-
-
-  renderer!.xr.addEventListener("sessionstart", async () => {
-    const referenceSpace = await session.requestReferenceSpace('local');
-    console.log("session and referenceSpace initialized.")
-    const onXRFrame = (time: DOMHighResTimeStamp, frame: XRFrame) => {
-      // Queue up the next draw request.
-      session.requestAnimationFrame(onXRFrame);
-
-      // Bind the graphics framebuffer to the baseLayer's framebuffer
-      gl!.bindFramebuffer(gl!.FRAMEBUFFER, session.renderState.baseLayer!.framebuffer)
-
-      // Retrieve the pose of the device.
-      // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
-      const pose = frame.getViewerPose(referenceSpace);
-      if (pose) {
-        // In mobile AR, we only have one view.
-        const view = pose.views[0];
-
-        const viewport = session.renderState.baseLayer!.getViewport(view);
-
-        if (!viewport) {
-          console.error("Viewport not found.")
-          return
-        }
-
-        if (!renderer || !camera || !scene) {
-          console.error("Three.js components not found.")
-          return
-        }
-
-        renderer.setSize(viewport.width, viewport.height)
-
-        // Use the view's transform matrix and projection matrix to configure the THREE.camera.
-        camera.matrix.fromArray(view.transform.matrix)
-        camera.projectionMatrix.fromArray(view.projectionMatrix);
-        camera.updateMatrixWorld(true);
-
-        // Render the scene with THREE.WebGLRenderer.
-        renderer.render(scene, camera)
-      } else {
-        console.error("Viewer pose not found.")
-      }
-    }
-    console.log("AR session initialized.")
+  const onXRFrame = (time:DOMHighResTimeStamp, frame: XRFrame) => {
+    // Queue up the next draw request.
     session.requestAnimationFrame(onXRFrame);
-  });
 
+    // Bind the graphics framebuffer to the baseLayer's framebuffer
+    gl!.bindFramebuffer(gl!.FRAMEBUFFER, session.renderState.baseLayer!.framebuffer)
+
+    // Retrieve the pose of the device.
+    // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
+    const pose = frame.getViewerPose(referenceSpace);
+    if (pose) {
+      // In mobile AR, we only have one view.
+      const view = pose.views[0];
+
+      const viewport = session.renderState.baseLayer!.getViewport(view);
+      renderer!.setSize(viewport!.width, viewport!.height)
+
+      // Use the view's transform matrix and projection matrix to configure the THREE.camera.
+      camera.matrix.fromArray(view.transform.matrix)
+      camera.projectionMatrix.fromArray(view.projectionMatrix);
+      camera.updateMatrixWorld(true);
+
+      // Render the scene with THREE.WebGLRenderer.
+      renderer!.render(scene!, camera)
+    }
+  }
+  session.requestAnimationFrame(onXRFrame);
 }
+
 
 async function startAR() {
   try {
-    console.log("Starting AR...")
-
-    console.log(await navigator.xr!.isSessionSupported("immersive-ar"))
-
-    await activateXR()
     await init()
   } catch (error) {
     if (error instanceof NotSupportedError) {
